9a31b4b5c11fa01d55bbe790c0fa5d89fe1bb9d2
Pratik Dahal <mapratikdahal@gmail.com>
2024-09-11 17:53:20 -0500

    base case identified

--- AME/create_err.R
+++ AME/create_err.R
@@ -9,11 +9,12 @@ rho <- 0.5  # Correlation coefficient
 # Create covariance matrix
 cov_matrix <- sigma^2 * matrix(c(1, rho, rho, 1), nrow = 2)
 
+err <- cbind(c(1,2), c(3,4), c(5,6))
 
+E <- matrix(rep(NA, each=9), nrow=3)
 
-
-
-
+E[upper.tri(E)] <- err[1,]
+E[lower.tri(E)] <- err[2,]
 
 
 
